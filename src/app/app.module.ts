22ba70a621a951196961a8914412568afbc9a92a
Laxmi Anandache <laxmi.anandache@clarivate.com>
2022-09-16 11:00:18 +0530

    initial commit main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PaymentComponent } from './payment/payment.component';
+import { OrderComponent } from './order/order.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { AddMenuComponent } from './BranchManager/add-menu/add-menu.component';
+import { ShowMenuComponent } from './show-menu/show-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ProductListComponent,
+    PaymentComponent,
+    OrderComponent,
+    AddProductComponent,
+    EditProductComponent,
+    AddMenuComponent,
+    ShowMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/show-menu/show-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-menu',
+  templateUrl: './show-menu.component.html',
+  styleUrls: ['./show-menu.component.css']
+})
+export class ShowMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
